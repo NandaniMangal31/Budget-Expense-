3040c974501220de2dcffc837c71501432c24038
Nandani Mangal <nandanimangal7@gmail.com>
2026-06-09 14:22:39 +0530

    backend changes after deploy

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({origin:"https://smart-spending-frontend.vercel.app/", credentials:true}));
 app.use(express.json({limit:"50mb"}));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use("/api/auth/", authRoutes);
