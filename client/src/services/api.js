b5b3def94cdc54f38eb6d5ae9101af08547b14d0
Nandani Mangal <nandanimangal7@gmail.com>
2026-06-09 14:34:01 +0530

    changes related to cors

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://smart-spending-backend.onrender.com",
+  baseURL: "https://smart-spending-backend.onrender.com/api",
 });
 API.interceptors.request.use(
   (config) => {
