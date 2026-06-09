84f81998e8b077f8f0eb8a81cd1f87a3af5e6d86
Nandani Mangal <nandanimangal7@gmail.com>
2026-06-09 14:41:43 +0530

    changes

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ connectDB();
 
 const app = express();
 
-app.use(cors({origin:"https://smart-spending-frontend.vercel.app/", credentials:true}));
+app.use(cors({origin:["https://smart-spending-frontend.vercel.app/","https://smart-spending-frontend.vercel.app","http://localhost:5173","http://localhost:3000"], credentials:true}));
 app.use(express.json({limit:"50mb"}));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use("/api/auth/", authRoutes);
