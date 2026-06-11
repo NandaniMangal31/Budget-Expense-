168ab97fcad295b2e5a3b7c7aafe6b8010b99d58
Nandani Mangal <nandanimangal7@gmail.com>
2026-06-11 14:40:30 +0530

    changes

--- server/server.js
+++ server/server.js
@@ -39,7 +39,12 @@ app.get("/", (req, res) => {
   res.send("Backend is running 🚀");
 });
 const PORT = process.env.PORT || 5000;
-
+app.get("/api/test", (req, res) => {
+  res.json({
+    success: true,
+    message: "Backend is working"
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
